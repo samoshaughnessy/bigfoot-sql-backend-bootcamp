a67e0eac6d03273dc8041c7a273573c99f13153a
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2022-07-08 15:45:48 +0800

    finsihed comfortable

--- index.js
+++ index.js
@@ -2,22 +2,50 @@ import cors from "cors";
 import express from "express";
 
 import db from "./db/models/index.cjs";
-const { Sighting } = db;
+const { sighting } = db;
+console.log("ok", sighting);
 
 const PORT = 3000;
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Enable CORS access to this server
 app.use(cors());
 
 app.get("/sightings", async (req, res) => {
-  const sightings = await Sighting.findAll();
+  const sightings = await sighting.findAll();
   res.json(sightings);
 });
 
-app.get("/sightings/:sightingId", async (req, res) => {
-  const sighting = await Sighting.findByPk(req.params.sightingId);
-  res.json(sighting);
+app.get("/sighting/:sightingId", async (req, res) => {
+  const singleSighting = await sighting.findByPk(req.params.sightingId);
+  res.json(singleSighting);
+});
+
+app.put("/sighting/:id", async (req, res) => {
+  await sighting.update(
+    {
+      location: req.body.location,
+      notes: req.body.note,
+      date: req.body.date,
+    },
+    {
+      where: { id: req.params.id },
+    }
+  );
+  res.send("Okay");
+});
+
+app.post("/sighting", async (req, res) => {
+  await sighting.create({
+    location: req.body.location,
+    notes: req.body.note,
+    date: req.body.date,
+  });
+
+  res.json({ message: "Success" });
 });
 
 app.listen(PORT, () => {
